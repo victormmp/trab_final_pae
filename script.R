392f96f1a1aec1e5cf9ce998ae00f79ba67d4792
victormmp <victormmp@outlook.com>
2019-12-03 00:30:40 -0300

    Adiciona scripts com análise

--- script.R
+++ script.R
@@ -21,7 +21,7 @@ data.decade <- with(data,
 
 # ANÁLISE FATORIAL
 
-model <- aov(number ~ month*state,
+model <- aov(number ~ year,
              data=data)
 
 replications(number ~ month*state,
@@ -52,3 +52,75 @@ plot(confint(mcp.inter),
 
 shapiro.test(model2$residuals[1:5000])
 
+#####################################################
+
+library(dplyr)
+
+group_by(data, month) %>%
+    summarise(
+        count = n(),
+        mean = mean(number, na.rm = TRUE),
+        sd = sd(number, na.rm = TRUE),
+        median = median(number, na.rm = TRUE),
+        IQR = IQR(number, na.rm = TRUE)
+    )
+
+data.month <- with(data,
+                    aggregate(x = number,
+                              by = list(month, year),
+                              FUN = sum
+                    )
+)
+
+m <- kruskal.test(number ~ month,
+                  data=data)
+
+pairwise.wilcox.test(data$number, data$month,
+                     p.adjust.method = "BH")
+
+
+library("ggpubr")
+ggboxplot(data, x = "month", y = "number",
+          color = "month",
+          ylab = "Ocorrencias", xlab = "Mes")
+
+ggline(data, x = "month", y = "number",
+       add = c("mean_se", "jitter"),
+       ylab = "Ocorrencias", xlab = "Mes")
+
+############################################################
+
+library(dplyr)
+
+group_by(data, year) %>%
+    summarise(
+        count = n(),
+        mean = mean(number, na.rm = TRUE),
+        sd = sd(number, na.rm = TRUE),
+        median = median(number, na.rm = TRUE),
+        IQR = IQR(number, na.rm = TRUE)
+    )
+
+
+m.year <- kruskal.test(number ~ year,
+                  data=data)
+
+pairwise.wilcox.test(data$number, data$year,
+                     p.adjust.method = "BH")
+
+pairwise.wilcox.test(data$number, data$year,
+                     p.adjust.method = "BH",
+                     alternative="less")
+
+pairwise.wilcox.test(data$number, data$year,
+                     p.adjust.method = "BH",
+                     alternative="greater")
+
+library("ggpubr")
+ggboxplot(data, x = "year", y = "number",
+          color = "year",
+          ylab = "Ocorrencias", xlab = "Abo")
+
+ggline(data, x = "year", y = "number",
+       add = c("mean_se", "jitter"),
+       ylab = "Ocorrencias", xlab = "Ano")
